20b0f175d014d5dec1107c99a41ec8bcc2063de7
Akhil Mohan <akhil2@illinois.edu>
2018-02-28 19:00:58 -0600

    non-zero progress

--- sphere.h
+++ sphere.h
@@ -6,7 +6,7 @@
 class sphere: public hitable  {
     public:
         sphere() {}
-        sphere(vec3 cen, float r, material *m) : center(cen), radius(r), mat_ptr(m)  {};
+        sphere(vec3 cen, float r) : center(cen), radius(r) {};
         virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         vec3 center;
         float radius;
